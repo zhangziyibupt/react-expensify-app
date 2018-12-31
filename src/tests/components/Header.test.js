b4f639d4b25a536a180c05f066d3781c5a8270ae
Ziyi Zhang <zhangziyibupt@gmail.com>
2018-12-31 15:07:24 -0800

    Add PrivateRoute

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 // shallow vs full DOM rendering(will render children components too)
 import { shallow } from 'enzyme';
 // import toJSON from 'enzyme-to-json'; // simplify the snapshot, see jest.config.json snapshotSerializers
 // import "../setupTests";
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {}}/>);
     // expect(toJSON(wrapper)).toMatchSnapshot();
     expect(wrapper).toMatchSnapshot();
     // expect(wrapper.find('h1').text()).toBe('Expensify');
@@ -17,4 +17,11 @@ test('should render Header correctly', () => {
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 });
